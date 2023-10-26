edccdcc9da310a1ce3bc83453961d2312275da60
Fernando Espinoza <rene.espinoza@catolica.edu.sv>
2023-10-26 10:40:50 -0600

    Avance 1

--- Admin-Alumnos/Form1.Designer.cs
+++ Admin-Alumnos/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtb1 = new System.Windows.Forms.TextBox();
+            this.txtb2 = new System.Windows.Forms.TextBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtb1
+            // 
+            this.txtb1.Location = new System.Drawing.Point(122, 122);
+            this.txtb1.Multiline = true;
+            this.txtb1.Name = "txtb1";
+            this.txtb1.Size = new System.Drawing.Size(155, 27);
+            this.txtb1.TabIndex = 0;
+            // 
+            // txtb2
+            // 
+            this.txtb2.Location = new System.Drawing.Point(122, 187);
+            this.txtb2.Multiline = true;
+            this.txtb2.Name = "txtb2";
+            this.txtb2.Size = new System.Drawing.Size(155, 27);
+            this.txtb2.TabIndex = 1;
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(73, 122);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(43, 13);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "Usuario";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(55, 187);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(61, 13);
+            this.lbl2.TabIndex = 3;
+            this.lbl2.Text = "Contraseña";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(402, 328);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.txtb2);
+            this.Controls.Add(this.txtb1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtb1;
+        private System.Windows.Forms.TextBox txtb2;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
     }
 }
 
